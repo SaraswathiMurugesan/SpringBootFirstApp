b9f1e8dfbfa6a9ed203e411e3bfff4361f70c5ff
SaraswathiMurugesan <SaraswathiMurugesan@gmail.com>
2020-08-23 22:15:57 -0400

    Added a Employee method

--- src/main/java/com/javabunga/springbootexample/web/HelloWorldController.java
+++ src/main/java/com/javabunga/springbootexample/web/HelloWorldController.java
@@ -1,15 +1,31 @@
 package com.javabunga.springbootexample.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.javabunga.springbootexample.model.Employee;
 
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping("/hello")
+    @RequestMapping("/")
+    public String hello(){
+        return "Hello World!!";
+    }
+    @RequestMapping(value="/hello", method = RequestMethod.GET)
     public String sayHello(@RequestParam(value = "name") String name) {
         return "Hello " + name + "!";
     }
 
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
+    public Employee firstService(@RequestParam int id) {
+
+        Employee emp = new Employee();
+        emp.setName("emp1");
+        emp.setDesignation("manager");
+        emp.setEmpId(id);
+        emp.setSalary(3000);
+        return emp;
+    }
 }
